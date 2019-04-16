ea6ef623313f0bf75a660145904ee4ba867cd268
WANG CHAO <1229983126@qq.com>
2019-04-16 22:42:46 +0800

    Update generated files for v1.9rc

--- static/js/v_summary.js
+++ static/js/v_summary.js
@@ -1,22 +1,17 @@
-function comparator(fn) {
-  return function compare(a, b) {
-    const a1 = fn(a);
-    const b1 = fn(b);
-    if (a1 === b1) {
-      return 0;
-    } if (a1 < b1) {
-      return -1;
-    }
-    return 1;
-  };
-}
+window.comparator = (fn) => function compare(a, b) {
+  const a1 = fn(a).toLowerCase ? fn(a).toLowerCase() : fn(a);
+  const b1 = fn(b).toLowerCase ? fn(b).toLowerCase() : fn(b);
+  if (a1 === b1) {
+    return 0;
+  } if (a1 < b1) {
+    return -1;
+  }
+  return 1;
+};
 
 // date functions //
 const DAY_IN_MS = (1000 * 60 * 60 * 24);
-function getIntervalDay(a, b) {
-  const diff = Date.parse(a) - Date.parse(b);
-  return diff / DAY_IN_MS;
-}
+const WEEK_IN_MS = DAY_IN_MS * 7;
 function getDateStr(date) {
   return (new Date(date)).toISOString().split('T')[0];
 }
@@ -46,10 +41,16 @@ window.vSummary = {
     return {
       filtered: [],
       filterSearch: '',
-      filterSort: 'displayName',
       filterSortReverse: false,
-      filterGroupRepos: true,
+      filterGroupSelection: 'groupByRepos',
+      sortGroupSelection: 'searchPath', // UI for sorting groups
+      sortWithinGroupSelection: 'name', // UI for sorting within groups
+      sortingOption: '',
+      isSortingDsc: '',
+      sortingWithinOption: '',
+      isSortingWithinDsc: '',
       filterTimeFrame: 'day',
+      filterBreakdown: false,
       tmpFilterSinceDate: '',
       tmpFilterUntilDate: '',
       filterSinceDate: '',
@@ -58,32 +59,40 @@ window.vSummary = {
       rampSize: 0.01,
       minDate: '',
       maxDate: '',
+      contributionBarColors: {},
     };
   },
   watch: {
     repos() {
       this.getFiltered();
     },
-    filterSort() {
+    sortGroupSelection() {
+      this.getFiltered();
+    },
+    sortWithinGroupSelection() {
       this.getFiltered();
     },
     filterSortReverse() {
       this.getFiltered();
     },
-    filterGroupRepos() {
-      this.getFiltered();
-    },
     filterTimeFrame() {
       this.getFiltered();
     },
+    filterGroupSelection() {
+      this.updateSortSelection();
+      this.getFiltered();
+    },
+    filterBreakdown() {
+      this.getFiltered();
+    },
     tmpFilterSinceDate() {
-      if (this.tmpFilterSinceDate >= this.minDate) {
+      if (this.tmpFilterSinceDate && this.tmpFilterSinceDate >= this.minDate) {
         this.filterSinceDate = this.tmpFilterSinceDate;
         this.getFiltered();
       }
     },
     tmpFilterUntilDate() {
-      if (this.tmpFilterUntilDate <= this.maxDate) {
+      if (this.tmpFilterUntilDate && this.tmpFilterUntilDate <= this.maxDate) {
         this.filterUntilDate = this.tmpFilterUntilDate;
         this.getFiltered();
       }
@@ -106,20 +115,21 @@ window.vSummary = {
       return totalCommits / totalCount;
     },
     avgContributionSize() {
-      if (typeof meanContributionSize === 'undefined') {
-        let totalLines = 0;
-        let totalCount = 0;
-        this.repos.forEach((repo) => {
-          repo.users.forEach((user) => {
-            if (user.totalCommits > 0) {
-              totalCount += 1;
-              totalLines += user.totalCommits;
-            }
-          });
+      let totalLines = 0;
+      let totalCount = 0;
+      this.repos.forEach((repo) => {
+        repo.users.forEach((user) => {
+          if (user.totalCommits > 0) {
+            totalCount += 1;
+            totalLines += user.totalCommits;
+          }
         });
-        meanContributionSize = totalLines / totalCount;
-      }
-      return meanContributionSize;
+      });
+
+      return totalLines / totalCount;
+    },
+    filteredRepos() {
+      return this.filtered.filter((repo) => repo.length > 0);
     },
   },
   methods: {
@@ -132,21 +142,75 @@ window.vSummary = {
       const newSize = 100 * (slice.insertions / this.avgCommitSize);
       return Math.max(newSize * this.rampSize, 0.5);
     },
-    getSliceTitle(slice) {
-      return `contribution on ${slice.sinceDate}: ${slice.insertions} lines`;
+    // position for commit granularity
+    getCommitPos(i, total, sinceDate, untilDate) {
+      return (total - i - 1) * DAY_IN_MS / total
+          / (this.getTotalForPos(sinceDate, untilDate) + DAY_IN_MS);
+    },
+    // position for day granularity
+    getSlicePos(date, sinceDate, untilDate) {
+      const total = this.getTotalForPos(sinceDate, untilDate);
+      return (new Date(untilDate) - new Date(date)) / (total + DAY_IN_MS);
+    },
+    // get duration in miliseconds between 2 date
+    getTotalForPos(sinceDate, untilDate) {
+      return new Date(untilDate) - new Date(sinceDate);
+    },
+    getSliceColor(date) {
+      const timeMs = (new Date(date)).getTime();
+      return (timeMs / DAY_IN_MS) % 5;
     },
     getSliceLink(user, slice) {
       const { REPOS } = window;
-      const untilDate = this.filterTimeFrame === 'week' ? addDays(slice.sinceDate, 6): slice.sinceDate;
+      const untilDate = this.filterTimeFrame === 'week' ? addDays(slice.date, 6) : slice.date;
 
       return `http://github.com/${
         REPOS[user.repoId].location.organization}/${
         REPOS[user.repoId].location.repoName}/commits/${
         REPOS[user.repoId].branch}?`
                 + `author=${user.name}&`
-                + `since=${slice.sinceDate}'T'00:00:00+08:00&`
+                + `since=${slice.date}'T'00:00:00+08:00&`
                 + `until=${untilDate}'T'23:59:59+08:00`;
     },
+    getFileFormatContributionBars(fileFormatContribution) {
+      let totalWidth = 0;
+      const contributionLimit = (this.avgContributionSize * 2);
+      const totalBars = {};
+      const maxLength = 100;
+
+      Object.keys(fileFormatContribution).forEach((fileFormat) => {
+        const contribution = fileFormatContribution[fileFormat];
+        const res = [];
+        let fileFormatWidth = 0;
+
+        // compute 100% width bars
+        const cnt = parseInt(contribution / contributionLimit, 10);
+        for (let cntId = 0; cntId < cnt; cntId += 1) {
+          res.push(maxLength);
+          fileFormatWidth += maxLength;
+          totalWidth += maxLength;
+        }
+
+        // compute < 100% width bars
+        const last = (contribution % contributionLimit) / contributionLimit;
+        if (last !== 0) {
+          res.push(last * maxLength);
+          fileFormatWidth += last * maxLength;
+          totalWidth += last * maxLength;
+        }
+
+        // split > 100% width bars into smaller bars
+        if ((totalWidth > maxLength) && (totalWidth !== fileFormatWidth)) {
+          res.unshift(maxLength - (totalWidth - fileFormatWidth));
+          res[res.length - 1] = res[res.length - 1] - (maxLength - (totalWidth - fileFormatWidth));
+          totalWidth = res[res.length - 1];
+        }
+        totalBars[fileFormat] = res;
+      });
+
+      return totalBars;
+    },
+
     getContributionBars(totalContribution) {
       const res = [];
       const contributionLimit = (this.avgContributionSize * 2);
@@ -168,33 +232,36 @@ window.vSummary = {
     updateFilterSearch(evt) {
       this.filterSearch = evt.target.value;
     },
-    getFilterHash() {
-      const { addHash } = window;
+    setSummaryHash() {
+      const { addHash, encodeHash } = window;
 
       addHash('search', this.filterSearch);
-      addHash('sort', this.filterSort);
+      addHash('sort', this.sortGroupSelection);
+      addHash('sortWithin', this.sortWithinGroupSelection);
 
       addHash('since', this.filterSinceDate);
       addHash('until', this.filterUntilDate);
       addHash('timeframe', this.filterTimeFrame);
 
       addHash('reverse', this.filterSortReverse);
-      addHash('repoSort', this.filterGroupRepos);
+      addHash('groupSelect', this.filterGroupSelection);
+      addHash('breakdown', this.filterBreakdown);
+
+      encodeHash();
     },
     renderFilterHash() {
-      const params = window.location.hash.slice(1).split('&');
-      params.forEach((param) => {
-        const [key, val] = param.split('=');
-        if (key) {
-          window.hashParams[key] = decodeURIComponent(val);
-        }
-      });
+      window.decodeHash();
 
-      const convertBool = txt => (txt === 'true');
+      const convertBool = (txt) => (txt === 'true');
       const hash = window.hashParams;
 
       if (hash.search) { this.filterSearch = hash.search; }
-      if (hash.sort) { this.filterSort = hash.sort; }
+      if (hash.sort) {
+        this.sortGroupSelection = hash.sort;
+      }
+      if (hash.sortWithin) {
+        this.sortWithinGroupSelection = hash.sortWithin;
+      }
 
       if (hash.timeframe) { this.filterTimeFrame = hash.timeframe; }
       if (hash.since) {
@@ -205,7 +272,13 @@ window.vSummary = {
       }
 
       if (hash.reverse) { this.filterSortReverse = convertBool(hash.reverse); }
-      if (hash.repoSort) { this.filterGroupRepos = convertBool(hash.repoSort); }
+      if (hash.groupSelect) {
+        this.filterGroupSelection = hash.groupSelect;
+      }
+      if (hash.breakdown) {
+        this.filterBreakdown = convertBool(hash.breakdown);
+      }
+      window.decodeHash();
     },
 
     getDates() {
@@ -213,26 +286,11 @@ window.vSummary = {
         return;
       }
 
-      let minDate = '';
-      let maxDate = '';
-      this.filtered.forEach((repo) => {
-        repo.forEach((user) => {
-          const { commits } = user;
-          if (commits.length) {
-            const date1 = commits[0].sinceDate;
-            const date2 = commits[commits.length - 1].sinceDate;
-            if (!minDate || minDate > date1) {
-              minDate = date1;
-            }
-            if (!maxDate || maxDate < date2) {
-              maxDate = date2;
-            }
-          }
-        });
-      });
+      const minDate = window.app.sinceDate;
+      const maxDate = window.app.untilDate;
 
       if (!this.filterSinceDate) {
-        if(!this.tmpFilterSinceDate || this.tmpFilterSinceDate < minDate){
+        if (!this.tmpFilterSinceDate || this.tmpFilterSinceDate < minDate) {
           this.tmpFilterSinceDate = minDate;
         }
 
@@ -241,16 +299,17 @@ window.vSummary = {
       }
 
       if (!this.filterUntilDate) {
-        if(!this.tmpFilterUntilDate || this.tmpFilterUntilDate > maxDate){
+        if (!this.tmpFilterUntilDate || this.tmpFilterUntilDate > maxDate) {
           this.tmpFilterUntilDate = maxDate;
         }
 
         this.filterUntilDate = maxDate;
         this.maxDate = maxDate;
       }
+      this.$emit('get-dates', [this.minDate, this.maxDate]);
     },
     getFiltered() {
-      this.getFilterHash();
+      this.setSummaryHash();
 
       // array of array, sorted by repo
       const full = [];
@@ -261,9 +320,9 @@ window.vSummary = {
         // filtering
         repo.users.forEach((user) => {
           const toDisplay = this.filterSearch.toLowerCase()
-            .split(' ').filter(param => param)
-            .map(param => user.searchPath.search(param) > -1)
-            .reduce((curr, bool) => curr || bool, false);
+              .split(' ').filter((param) => param)
+              .map((param) => user.searchPath.search(param) > -1)
+              .reduce((curr, bool) => curr || bool, false);
 
           if (!this.filterSearch || toDisplay) {
             this.getUserCommits(user);
@@ -284,26 +343,54 @@ window.vSummary = {
       this.getDates();
       this.sortFiltered();
     },
+    processFileFormats() {
+      const selectedColors = ['#ffe119', '#4363d8', '#3cb44b', '#f58231', '#911eb4', '#46f0f0', '#f032e6',
+          '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1',
+          '#000075', '#808080'];
+      const colors = {};
+      let i = 0;
+
+      this.repos.forEach((repo) => {
+        const user = repo.users[0];
+        Object.keys(user.fileFormatContribution).forEach((fileFormat) => {
+          if (!Object.prototype.hasOwnProperty.call(colors, fileFormat)) {
+            colors[fileFormat] = selectedColors[i];
+            i = (i + 1) % selectedColors.length;
+          }
+        });
+        this.contributionBarColors = colors;
+      });
+    },
     splitCommitsWeek(user) {
       const { commits } = user;
-      const leng = commits.length;
 
       const res = [];
-      for (let weekId = 0; weekId < (leng - 1) / 7; weekId += 1) {
+
+      const sinceDate = dateRounding(this.filterSinceDate, 1);
+      const untilDate = this.filterUntilDate;
+
+      const sinceMs = (new Date(sinceDate)).getTime();
+      const untilMs = (new Date(untilDate)).getTime();
+
+      const diff = Math.round(Math.abs((untilMs - sinceMs) / DAY_IN_MS));
+
+      for (let weekId = 0; weekId < diff / 7; weekId += 1) {
+        const startOfWeekMs = sinceMs + (weekId * WEEK_IN_MS);
+
         const week = {
           insertions: 0,
           deletions: 0,
-          sinceDate: commits[weekId * 7].sinceDate,
-          untilDate: '',
+          date: getDateStr(startOfWeekMs),
         };
 
-        for (let dayId = 0; dayId < 7; dayId += 1) {
-          const commit = commits[(weekId * 7) + dayId];
-          if (commit) {
-            week.insertions += commit.insertions;
-            week.deletions += commit.deletions;
-            week.untilDate = commit.untilDate;
-          }
+        // commits are not contiguous, meaning there are gaps of days without
+        // commits, so we are going to check each commit's date and make sure
+        // it is within the duration of a week
+        while (commits.length > 0
+            && (new Date(commits[0].date)).getTime() < startOfWeekMs + WEEK_IN_MS) {
+          const commit = commits.shift();
+          week.insertions += commit.insertions;
+          week.deletions += commit.deletions;
         }
 
         res.push(week);
@@ -321,82 +408,127 @@ window.vSummary = {
       }
 
       let sinceDate = this.filterSinceDate;
-      if (!sinceDate) {
-        ({ sinceDate } = userFirst);
+      if (!sinceDate || sinceDate === 'undefined') {
+        sinceDate = userFirst.date;
       }
 
       let untilDate = this.filterUntilDate;
       if (!untilDate) {
-        untilDate = userLast.sinceDate;
-      }
-
-      if (this.filterTimeFrame === 'week') {
-        sinceDate = dateRounding(sinceDate, 1);
-      }
-      let diff = getIntervalDay(userFirst.sinceDate, sinceDate);
-
-      const startMs = (new Date(sinceDate)).getTime();
-      for (let dayId = 0; dayId < diff; dayId += 1) {
-        user.commits.push({
-          insertions: 0,
-          deletions: 0,
-          sinceDate: getDateStr(startMs + (dayId * DAY_IN_MS)),
-          untilDate: getDateStr(startMs + ((dayId + 1) * DAY_IN_MS)),
-        });
+        untilDate = userLast.date;
       }
 
       user.dailyCommits.forEach((commit) => {
-        const date = commit.sinceDate;
+        const { date } = commit;
         if (date >= sinceDate && date <= untilDate) {
           user.commits.push(commit);
         }
       });
 
-      if (this.filterTimeFrame === 'week') {
-        untilDate = dateRounding(untilDate);
-      }
-      diff = getIntervalDay(untilDate, userLast.sinceDate);
-
-      const endMs = (new Date(userLast.sinceDate)).getTime();
-      for (let paddingId = 1; paddingId < diff; paddingId += 1) {
-        user.commits.push({
-          insertions: 0,
-          deletions: 0,
-          sinceDate: getDateStr(endMs + (paddingId * DAY_IN_MS)),
-          untilDate: getDateStr(endMs + ((paddingId + 1) * DAY_IN_MS)),
-        });
-      }
-
       return null;
     },
-    sortFiltered() {
-      const full = [];
-      if (!this.filterGroupRepos) {
-        full.push([]);
-      }
-
-      this.filtered.forEach((users) => {
-        if (this.filterGroupRepos) {
-          users.sort(comparator(ele => ele[this.filterSort]));
-          full.push(users);
-        } else {
-          users.forEach(user => full[0].push(user));
+    updateSortSelection() {
+      this.getOptionWithOrder();
+      // Update UI selection to change all illegal options
+      if (this.filterGroupSelection === 'groupByAuthors') {
+        if (!this.sortWithinGroupSelection || this.sortingWithinOption === 'name') {
+          this.sortWithinGroupSelection = 'searchPath';
         }
-      });
-
-      if (!this.filterGroupRepos) {
-        full[0].sort(comparator(ele => ele[this.filterSort]));
+        if (this.sortingOption !== 'name') {
+          this.sortGroupSelection = 'name';
+        }
+      } else if (this.filterGroupSelection === 'groupByRepos') {
+        if (!this.sortWithinGroupSelection || this.sortingWithinOption === 'searchPath') {
+          this.sortWithinGroupSelection = 'name';
+        }
+        if (this.sortingOption !== 'searchPath') {
+          this.sortGroupSelection = 'searchPath';
+        }
+      } else if (this.filterGroupSelection === 'groupByNone') {
+        this.sortWithinGroupSelection = '';
+      }
+    },
+    getOptionWithOrder() {
+      [this.sortingOption, this.isSortingDsc] = this.sortGroupSelection.split(' ');
+      [this.sortingWithinOption, this.isSortingWithinDsc] = this.sortWithinGroupSelection.split(' ');
+    },
+    sortFiltered() {
+      this.getOptionWithOrder();
+      let full = [];
+      if (this.filterGroupSelection === 'groupByNone') {
+        // push all repos into the same group
+        full[0] = this.groupByNone(this.filtered);
+      } else if (this.filterGroupSelection === 'groupByAuthors') {
+        full = this.groupByAuthors(this.filtered);
+      } else {
+        full = this.groupByRepos(this.filtered);
       }
 
       if (this.filterSortReverse) {
-        full.forEach(repo => repo.reverse());
+        full.forEach((repo) => repo.reverse());
       }
 
       this.filtered = full;
+    },
+
+    groupByRepos(repos) {
+      const sortedRepos = [];
+      repos.forEach((users) => {
+        users.sort(window.comparator((ele) => ele[this.sortingWithinOption]));
+        if (this.isSortingWithinDsc) {
+          users.reverse();
+        }
+        sortedRepos.push(users);
+      });
+      sortedRepos.sort(window.comparator((repo) => repo[0][this.sortingOption]));
+      if (this.isSortingDsc) {
+        sortedRepos.reverse();
+      }
+      return sortedRepos;
+    },
+    groupByNone(repos) {
+      const sortedRepos = [];
+      repos.forEach((users) => {
+        users.forEach((user) => {
+          sortedRepos.push(user);
+        });
+      });
+      sortedRepos.sort(window.comparator((ele) => ele[this.sortingOption]));
+      if (this.isSortingDsc) {
+        sortedRepos.reverse();
+      }
+
+      return sortedRepos;
+    },
+    groupByAuthors(repos) {
+      const authorMap = {};
+      const filtered = [];
+      repos.forEach((users) => {
+        users.forEach((user) => {
+          if (Object.keys(authorMap).includes(user.name)) {
+            authorMap[user.name].push(user);
+          } else {
+            authorMap[user.name] = [user];
+          }
+        });
+      });
+      Object.keys(authorMap).forEach((author) => {
+        authorMap[author].sort(window.comparator((repo) => repo[this.sortingWithinOption]));
+        if (this.isSortingWithinDsc) {
+          authorMap[author].reverse();
+        }
+        filtered.push(authorMap[author]);
+      });
+
+      filtered.sort(window.comparator((ele) => ele[0][this.sortingOption]));
+      if (this.isSortingDsc) {
+        filtered.reverse();
+      }
+      return filtered;
     },
   },
   created() {
     this.renderFilterHash();
     this.getFiltered();
+    this.processFileFormats();
   },
 };
